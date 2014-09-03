546c4c1ad6ee03caaeff062465e961a48f565670
Maarten Claes <maartencls@gmail.com>
2014-09-03 21:36:18 +0200

    Cleanup test

--- tree/printer_test.go
+++ tree/printer_test.go
@@ -2,38 +2,91 @@ package tree
 
 import (
 	"io/ioutil"
+	"os"
+	"sort"
 	"strings"
 	"testing"
 )
 
-func TestPrintingDir(t *testing.T) {
-	f, _ := ioutil.TempFile("", "testing")
-	p := &Printer{
-		ShowAll:      false,
-		ShowOnlyDirs: false,
-		Out:          f}
-	node, _ := NewNode(".")
+func SetupDirStructure(dirs map[string]os.FileMode) {
+	base := BaseTestPath()
+	// Reset test directory
+	os.RemoveAll(base)
+	os.MkdirAll(base, 0777)
+
+	keys := make([]string, 0, len(dirs))
+	for d, _ := range dirs {
+		keys = append(keys, d)
+	}
+	sort.Strings(keys)
+
+	for _, name := range keys {
+		path := base + "/" + name
+		mode := dirs[name]
+		if strings.HasSuffix(path, "/") {
+			os.MkdirAll(path, mode)
+		} else {
+			os.Create(path)
+			os.Chmod(path, mode)
+		}
+	}
+}
+
+func AssertTreeMatches(t *testing.T, p *Printer, dirs map[string]os.FileMode, expectedLines []string) {
+	SetupDirStructure(dirs)
+
+	// Print tree
+	out, _ := ioutil.TempFile("", "testing")
+	p.Out = out
+	node, _ := NewNode(BaseTestPath())
 	p.Print(node, 0)
-
-	content, _ := ioutil.ReadFile(f.Name())
+	content, _ := ioutil.ReadFile(out.Name())
 	actual := string(content)
-
-	lines := []string{
-		".",
-		"|-- node.go",
-		"|-- printer.go",
-		"|-- printer_test.go",
-		"`-- style.go"}
-
 	actualLines := strings.Split(actual, "\n")
-	for i, line := range lines {
+
+	// Compare to expected output
+	for i, line := range expectedLines {
+		if i == 0 {
+			continue
+		}
 		if i < len(actualLines) {
 			actualLine := actualLines[i]
 			if line != actualLine {
-				t.Errorf("Expected actualLines[%d] to == '%s' was '%s'", i, line, actualLine)
+				msg := strings.Join([]string{
+					"Expected actualLines[%d]",
+					"to be  : %q",
+					"but was: %q",
+				}, "\n")
+				t.Errorf(msg, i, line, actualLine)
+				return
 			}
 		} else {
-			t.Errorf("Expected actualLines[%d] to == '%s' was out of bounds", i, line)
+			t.Errorf("Expected actualLines[%d] to be %q, but was out of bounds", i, line)
+			return
 		}
 	}
+}
+
+func BaseTestPath() string {
+	return os.TempDir() + "tmp/tests"
+}
+
+func TestPrintingDir(t *testing.T) {
+	dirs := map[string]os.FileMode{
+		"mydir/":         0777,
+		"mydir/stuff.md": 0777,
+		"mydir/thing.md": 0777,
+		"hello.txt":      0777,
+		"world.txt":      0777,
+	}
+	expectedTree := []string{
+		".",
+		"|-- hello.txt",
+		"|-- mydir/",
+		"|   |-- stuff.md",
+		"|   `-- thing.md",
+		"`-- world.txt",
+	}
+	p := &Printer{ShowAll: false, ShowOnlyDirs: false}
+	AssertTreeMatches(t, p, dirs, expectedTree)
 }
